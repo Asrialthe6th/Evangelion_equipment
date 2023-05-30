8688d25259839450c4a012acec600780775a1e08
Asrialthe6th <asrialthe6th@gmail.com>
2023-05-30 10:27:51 -0500

    Added placeholder textures for s2 drives. Added creative mode tab, the tab may or may not have the correct name.

---
+++ src/main/java/net/asrialthe6th/evangelion/item/ModCreativeModeTab.java
@@ -0,0 +1,14 @@
+package net.asrialthe6th.evangelion.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModeTab
+{
+    public static final CreativeModeTab EVANGELION_TAB = new CreativeModeTab("evangelionmodtab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.S2_DRIVE.get());
+        }
+    };
+}
--- src/main/java/net/asrialthe6th/evangelion/item/ModItems.java
+++ src/main/java/net/asrialthe6th/evangelion/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.asrialthe6th.evangelion.item;
 
 import net.asrialthe6th.evangelion.Evangelion;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,9 +13,9 @@ public class ModItems
             DeferredRegister.create(ForgeRegistries.ITEMS, Evangelion.MOD_ID);
 
     public static final RegistryObject<Item> S2_DRIVE = ITEMS.register("s2_drive",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.EVANGELION_TAB)));
     public static final RegistryObject<Item> AWAKENED_S2_DRIVE = ITEMS.register("awakened_s2_drive",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.EVANGELION_TAB)));
 
     public static void register(IEventBus eventBus)
     {
